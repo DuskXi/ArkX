815e12ae0e0269fe94574b0f8105b4283f74667f
DuskXi <84715902+DuskXi@users.noreply.github.com>
2022-02-08 00:54:50 +0000

    支持多设备，以及其他调整

--- Device/viewer.py
+++ Device/viewer.py
@@ -22,13 +22,17 @@ class AndroidViewer(ControlMixin):
     received = 0
     video_data_queue = Queue()
 
-    def __init__(self, max_width=3840, bitrate=8000000, max_fps=8, adb_path='', ip='127.0.0.1', port=8081):
+    def __init__(self, max_width=3840, bitrate=8000000, max_fps=8, adb_path='', ip='127.0.0.1', port=8081, multipleDevices=False, deviceADBName=None):
         self.ip = ip
         self.port = port
 
         self.device_name = ""
 
         self.adb_path = adb_path
+
+        self.multipleDevices = multipleDevices
+        if multipleDevices:
+            self.deviceADBName = deviceADBName
 
         assert self.deploy_server(max_width, bitrate, max_fps)
 
@@ -47,7 +51,7 @@ class AndroidViewer(ControlMixin):
             self.video_data_queue.put(raw_h264)
 
     def init_server_connection(self):
-        logger.info("Connecting video socket")
+        logger.info("连接视频流套接字...")
         self.video_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.video_socket.connect((self.ip, self.port))
 
@@ -55,7 +59,7 @@ class AndroidViewer(ControlMixin):
         if not len(dummy_byte):
             raise ConnectionError("Did not receive Dummy Byte!")
 
-        logger.info("Connecting control socket")
+        logger.info("连接控制套接字")
 
         self.control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.control_socket.connect((self.ip, self.port))
@@ -63,13 +67,13 @@ class AndroidViewer(ControlMixin):
         device_name = self.video_socket.recv(64).decode("utf-8")
 
         if not len(device_name):
-            raise ConnectionError("Did not receive Device Name!")
-        logger.info("Device Name: " + device_name)
+            raise ConnectionError("未能成功获取设备名!")
+        logger.info("设备名: " + device_name)
         self.device_name = device_name
 
         res = self.video_socket.recv(4)
         self.resolution = struct.unpack(">HH", res)
-        logger.info("Screen resolution: " + str(self.resolution))
+        logger.info("屏幕分辨率: " + str(self.resolution))
         self.video_socket.setblocking(0)
 
     def deploy_server(self, max_width=1024, bitrate=8000000, max_fps=10):
@@ -79,26 +83,27 @@ class AndroidViewer(ControlMixin):
             server_root = os.path.abspath(os.path.dirname(__file__))
             server_file_path = server_root + '/scrcpy-server.jar'
             if not os.path.exists(server_file_path):
-                logger.critical(
-                    "Core control file is not found ! :" + server_file_path)
+                logger.error(
+                    "scrcpy-server.jar 丢失, 请检查目录:" + server_file_path)
                 raise FileNotFoundError(
-                    "Couldn't find ADB at path ADB_bin: " + str(server_file_path))
-            adb_push = subprocess.Popen([self.adb_path, 'push', server_file_path, '/data/local/tmp/'],
-                                        stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=server_root)
-            adb_push_comm = ''.join(
-                [x.decode("utf-8") for x in adb_push.communicate() if x is not None])
+                    "crcpy-server.jar 丢失, 请检查目录:" + str(server_file_path))
+            device_args = []
+            if self.multipleDevices:
+                device_args = ["-s", self.deviceADBName]
 
+            adb_push = subprocess.Popen([self.adb_path] + device_args + ['push', server_file_path, '/data/local/tmp/'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=server_root)
+            adb_push_comm = ''.join([x.decode("utf-8") for x in adb_push.communicate() if x is not None])
             if "error" in adb_push_comm:
                 logger.critical("Is your device/emulator visible to ADB?")
                 raise Exception(adb_push_comm)
 
-            logger.info("Running server...")
-            self.Subp[0] = subprocess.Popen([self.adb_path, 'shell',
-                                             'CLASSPATH=/data/local/tmp/scrcpy-server.jar',
-                                             'app_process', '/', 'com.genymobile.scrcpy.Server 1.12.1 {} {} {} true - false true'.format(max_width, bitrate, max_fps)],
+            logger.info("开始运行服务端...")
+            self.Subp[0] = subprocess.Popen([self.adb_path] + device_args +
+                                            ['shell', 'CLASSPATH=/data/local/tmp/scrcpy-server.jar', 'app_process', '/',
+                                             'com.genymobile.scrcpy.Server 1.12.1 {} {} {} true - false true'.format(max_width, bitrate, max_fps)],
                                             cwd=server_root)
-            logger.info("Forward server port...")
-            subprocess.Popen([self.adb_path, 'forward', 'tcp:8081', 'localabstract:scrcpy'], cwd=server_root).wait()
+            logger.info("转发端口...")
+            subprocess.Popen([self.adb_path] + device_args + ['forward', 'tcp:8081', 'localabstract:scrcpy'], cwd=server_root).wait()
             sleep(2)
         except FileNotFoundError:
             raise FileNotFoundError(
